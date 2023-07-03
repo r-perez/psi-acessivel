07b68df1d0f2a99218a9d2898fc6cad714a24867
Ariel Santos <ariel@fluente.software>
2023-07-03 19:29:22 -0300

    Download data from Google Drive

---
+++ prisma/migrations/20230703220455_optional_address/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Therapist" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT,
+    "telephone" TEXT NOT NULL,
+    "crp" TEXT NOT NULL,
+    "address" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Therapist" ("address", "createdAt", "crp", "email", "id", "name", "telephone", "updatedAt") SELECT "address", "createdAt", "crp", "email", "id", "name", "telephone", "updatedAt" FROM "Therapist";
+DROP TABLE "Therapist";
+ALTER TABLE "new_Therapist" RENAME TO "Therapist";
+CREATE UNIQUE INDEX "Therapist_email_key" ON "Therapist"("email");
+CREATE UNIQUE INDEX "Therapist_telephone_key" ON "Therapist"("telephone");
+CREATE UNIQUE INDEX "Therapist_crp_key" ON "Therapist"("crp");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,80 @@
 import { PrismaClient } from "@prisma/client";
+import { parse } from "csv-parse";
 
 const prisma = new PrismaClient();
 
 async function seed() {
+  const sheetId = process.env.DRIVE_SHEET_ID;
+
+  console.log(`Fetching data from Google Drive...`);
+  const response = await fetch(
+    `https://docs.google.com/spreadsheets/d/${sheetId}/export?format=csv`,
+    {
+      redirect: "follow",
+    }
+  );
+  const data = await response.text();
+
+  console.log(`Parsing CSV...`);
+  parse(
+    data,
+    {
+      columns: [
+        "name",
+        "phone",
+        "email",
+        "url",
+        "approaches",
+        "crp",
+        "address",
+        "notes",
+      ],
+      relaxColumnCountMore: true,
+      skipEmptyLines: true,
+    },
+    async (err, records) => {
+      if (err) {
+        console.error(err);
+      }
+
+      for (const record of records) {
+        if (!(record.crp && record.name && (record.phone || record.email))) {
+          continue;
+        }
+
+        let approaches = record.approaches.split(",")
+        if (approaches.length === 1) approaches = record.approaches.split("/")
+        if (approaches.length === 1) approaches = [record.approaches]
+        const approachesConnect = approaches.map((approach: string) => ({
+          where: { name: approach },
+          create: { name: approach },
+        }));
+
+        await prisma.therapist.upsert({
+          where: { crp: record.crp },
+          update: {
+            telephone: record.phone || null,
+            email: record.email || null,
+            approaches: {
+              connectOrCreate: approachesConnect,
+            },
+            address: record.address || null,
+          },
+          create: {
+            name: record.name,
+            telephone: record.phone || null,
+            email: record.email || null,
+            approaches: {
+              connectOrCreate: approachesConnect,
+            },
+            crp: record.crp,
+            address: record.address || null,
+          },
+        });
+      }
+    }
+  );
+
   console.log(`Database has been seeded. 🌱`);
 }
 
